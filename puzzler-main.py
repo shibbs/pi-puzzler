2a5aa40989413282d3fb557069d2a5afd6f58407
Shibbs <shibbs.a@gmail.com>
2020-11-27 21:17:56 -0700

    levels 9-11 are built, #neverforget

--- puzzler-main.py
+++ puzzler-main.py
@@ -1,5 +1,7 @@
 from external_levels import *
 import random
+import os.path
+from os import path
 
 FAIL = 0
 PASS = 1
@@ -16,7 +18,7 @@ def level1():
         return FAIL
 
 def level2():
-    print('welcome to level 2. The password for this one is hanging out in a file called level2.txt')
+    print('welcome to level 2. The password for this one is hanging out in a file called level2.txt. I recommend checking out nano, like whatever that is AIMRITE!?')
     resp = input()
     f = open("level2.txt", "r")
     pw = f.readline()
@@ -35,7 +37,7 @@ def level4_local():
 
 
 def level5():
-    print('That one was actually kind of tricky. Enough of that CS stuff, now let’s play with the command line a bit. If you poke around, there’s a file called “level5.png”. Turns out this thing can show pictures with a command called fbi... ')
+    print('That one was actually kind of tricky, not bad. Enough of that CS stuff, now let’s play with the command line a bit. If you poke around, there’s a file called “level5.png”. Turns out this thing can show pictures with a command called fbi... ')
     resp = input()
     if (resp == "IN_VISIBLE"):
         return PASS
@@ -63,24 +65,33 @@ def level8():
     else:
         return FAIL
 def level9():
-    print("This level not yet built. Shame on the game master")
+    print("All Right, welcome to Level9. To proceed onto the next level, you'll have to edit the correct file to have 'first_emo_best_emo' as the very first line. Hit Enter when you're ready for me to check your work")
     resp = input()
-    if (resp == "XX"):
+    f = open("why_is_it_called_a_folder_if_I_cant_fold_it/level9.txt", "r")
+    pw = f.readline()
+    pw = pw.rstrip()
+    f.close()
+    if (pw == "first_emo_best_emo"):
         return PASS
     else:
         return FAIL
 def level10():
-    print("This level not yet built. Shame on the game master")
+    print("Well that was cute, but do you know how to make a file from scratch? I'll be looking for a level10.txt file to appear where one might least expect to find a folder...")
     resp = input()
-    if (resp == "XX"):
+    #we're looking for level10.txt to show up in an_unfoldable_folder
+    if (path.exists("an_unfoldable_folder/level10.txt") ):
         return PASS
     else:
         return FAIL
 def level11():
-    print("This level not yet built. Shame on the game master")
+    print("Well fair enough, now you’ve gotta transport that file into a folder called magic_portal")
     resp = input()
-    if (resp == "XX"):
-        return PASS
+    if (path.exists("magic_portal/level10.txt") ):
+        if (path.exists("an_unfoldable_folder/level10.txt") ):
+            print("it's a magic portal, not a magic photocopier")
+            return FAIL
+        else:
+            return PASS
     else:
         return FAIL
 def level12():
@@ -122,13 +133,13 @@ def leveln( level_num):
     if(level_num == 8):
         return level8()
     if(level_num == 9):
-        return level8()
+        return level9()
     if(level_num == 10):
-        return level8()
+        return level10()
     if(level_num == 11):
-        return level8()
+        return level11()
     if(level_num == 12):
-        return level8()
+        return level12()
 
 
 print("Welcome to Steve puzzle box! Let's begin the puzzling...")
